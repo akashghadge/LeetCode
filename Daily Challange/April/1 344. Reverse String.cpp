886abe8b0a45acbb06f1e7351e10431ba34c706c
akash_ghadge <asghadge6@gmail.com>
2022-04-01 12:59:41 +0530

    LC Daily

---
+++ Daily Challange/April/1 344. Reverse String.cpp
@@ -0,0 +1,54 @@
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        reverse(s.begin(), s.end());
+    }
+};
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+
+    return 0;
+}
