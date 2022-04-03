1b9d039e842579e59013e0a18b88b65a4138f3a1
akash_ghadge <asghadge6@gmail.com>
2022-04-03 10:38:47 +0530

    LC Daily Done

---
+++ Daily Challange/April/3 31. Next Permutation.cpp
@@ -0,0 +1,94 @@
+/*
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &arr)
+    {
+        int N = arr.size();
+        if (N <= 1)
+        {
+            return;
+        }
+        int k, l, i;
+        for (i = N - 2; i >= 0; i--)
+        {
+            if (arr[i] >= arr[i + 1])
+            {
+                k = i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (i < 0)
+        {
+            sort(arr.begin(), arr.end());
+        }
+        else
+        {
+            k = i;
+            for (l = N - 1; l > k; l--)
+            {
+                if (arr[l] > arr[k])
+                {
+                    break;
+                }
+            }
+            swap(arr[l], arr[k]);
+            reverse(arr.begin() + k + 1, arr.end());
+        }
+        return;
+    }
+};
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+
+    return 0;
+}
