70566921a25987f4e54a6404bd075cbdcc29ac65
akash_ghadge <asghadge6@gmail.com>
2022-06-03 11:23:19 +0530

    LC Daily Done

---
+++ Daily Challange/June/4 51. N-Queens.cpp
@@ -0,0 +1,75 @@
+/*
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+
+
+Example 1:
+
+
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+Example 2:
+
+Input: n = 1
+Output: [["Q"]]
+
+
+Constraints:
+
+1 <= n <= 9
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    std::vector<std::vector<std::string>> solveNQueens(int n)
+    {
+        std::vector<std::vector<std::string>> res;
+        std::vector<std::string> nQueens(n, std::string(n, '.'));
+        solveNQueens(res, nQueens, 0, n);
+        return res;
+    }
+
+private:
+    void solveNQueens(std::vector<std::vector<std::string>> &res, std::vector<std::string> &nQueens, int row, int &n)
+    {
+        if (row == n)
+        {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (isValid(nQueens, row, col, n))
+            {
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, row + 1, n);
+                nQueens[row][col] = '.';
+            }
+    }
+    bool isValid(std::vector<std::string> &nQueens, int row, int col, int &n)
+    {
+        // check if the column had a queen before.
+        for (int i = 0; i != row; ++i)
+            if (nQueens[i][col] == 'Q')
+                return false;
+        // check if the 45° diagonal had a queen before.
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        // check if the 135° diagonal had a queen before.
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        return true;
+    }
+};
+int main()
+{
+}
