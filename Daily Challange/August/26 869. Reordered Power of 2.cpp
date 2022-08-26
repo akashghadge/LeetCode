5dcb22ff762d1071a50c4a3653ec77f59be40b87
akash_ghadge <asghadge6@gmail.com>
2022-08-26 12:47:41 +0530

    LC Daily Done

---
+++ Daily Challange/August/26 869. Reordered Power of 2.cpp
@@ -0,0 +1,50 @@
+/*
+You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
+
+Return true if and only if we can do this so that the resulting number is a power of two.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Example 2:
+
+Input: n = 10
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 109
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+bool reorderedPowerOf2(int n) {
+        string str = to_string(n);
+        sort(str.begin(),str.end());
+		
+        vector<string> power;
+        for(int i=0;i<=30;i++){
+            int p = pow(2,i);
+            power.push_back(to_string(p));
+        }
+        
+        for(int i=0;i<=30;i++){
+            sort(power[i].begin(),power[i].end());
+        }
+        
+        for(int i=0;i<=30;i++){
+            if(power[i] == str ) return true;
+        }
+        return false;
+    }
+
+};
+int main()
+{
+}
