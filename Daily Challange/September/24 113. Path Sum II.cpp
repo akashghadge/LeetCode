4b282322260881f6638031d0d918894b5edb20d5
akash_ghadge <asghadge6@gmail.com>
2022-09-24 20:20:02 +0530

    LC Daily Done

---
+++ Daily Challange/September/24 113. Path Sum II.cpp
@@ -0,0 +1,76 @@
+/*
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode*root,int targetSum,vector<int> &curr,vector<vector<int>>&ans)
+    {
+        if(!root)
+            return;
+        if(!root->left and !root->right)
+        {
+            curr.push_back(root->val);
+            if(targetSum-root->val==0)
+            {
+                ans.push_back(curr);
+            }
+            curr.pop_back();
+        }
+        curr.push_back(root->val);
+        dfs(root->left,targetSum-root->val,curr,ans);
+        dfs(root->right,targetSum-root->val,curr,ans);
+        curr.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        dfs(root,targetSum,curr,ans);
+        return ans;
+    }
+};
+int main()
+{
+}
