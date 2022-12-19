8bb941b2611d5474e4ba8e873eb6f711c18d9d59
akash_ghadge <asghadge6@gmail.com>
2022-12-19 23:35:18 +0530

    LC Daily Done

---
+++ Daily Challange/12_22 Dec/19 1971. Find if Path Exists in Graph.cpp
@@ -0,0 +1,69 @@
+/*
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+
+You want to determine if there is a valid path that exists from vertex source to vertex destination.
+
+Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+Output: true
+Explanation: There are two paths from vertex 0 to vertex 2:
+- 0 → 1 → 2
+- 0 → 2
+Example 2:
+
+
+Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], source = 0, destination = 5
+Output: false
+Explanation: There is no path from vertex 0 to vertex 5.
+ 
+
+Constraints:
+
+1 <= n <= 2 * 105
+0 <= edges.length <= 2 * 105
+edges[i].length == 2
+0 <= ui, vi <= n - 1
+ui != vi
+0 <= source, destination <= n - 1
+There are no duplicate edges.
+There are no self edges.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class UnionFind {
+    vector<int> id;
+public:
+    UnionFind(int n) : id(n) {
+        iota(begin(id), end(id), 0);
+    }
+    int find(int a) {
+        return id[a] == a ? a : (id[a] = find(id[a]));
+    }
+    void connect(int a, int b) {
+        id[find(a)] = find(b);
+    }
+    bool connected(int a, int b) {
+        return find(a) == find(b);
+    }
+};
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& E, int start, int end) {
+        UnionFind uf(n);
+        for (auto &e : E) {
+            uf.connect(e[0], e[1]);
+        }
+        return uf.connected(start, end);
+    }
+};
+
+int main()
+{
+}
