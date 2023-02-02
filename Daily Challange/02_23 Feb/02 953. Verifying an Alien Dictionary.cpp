1a241f268a3cca235a6d705c6171a8faa2dfbe0b
akash_ghadge <asghadge6@gmail.com>
2023-02-02 19:39:15 +0530

    LC Daily Done

---
+++ Daily Challange/02_23 Feb/02 953. Verifying an Alien Dictionary.cpp
@@ -0,0 +1,55 @@
+/*
+In an alien language, surprisingly, they also use English lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographically in this alien language.
+
+
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are English lowercase letters.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool isAlienSorted(vector<string> &words, const string &order)
+    {
+        vector<char> map(26); // Order = "bca" -> map = ['c', 'a', 'b']
+
+        for (int i = 0; i < 26; ++i)
+            map[order[i] - 'a'] = i + 'a';
+
+        for (string &word : words)
+            for (char &c : word)
+                c = map[c - 'a'];
+
+        return is_sorted(begin(words), end(words));
+    }
+};
+
+int main()
+{
+}
