ca2d19cbadfe4fe4e81b533c59a05614c386464e
akash_ghadge <asghadge6@gmail.com>
2023-04-04 17:24:19 +0530

    LC Daily Done

---
+++ Daily Challange/04_23 April/04 2405. Optimal Partition of String.cpp
@@ -0,0 +1,54 @@
+/*
+Given a string s, partition the string into one or more substrings such that the characters in each substring are unique. That is, no letter appears in a single substring more than once.
+
+Return the minimum number of substrings in such a partition.
+
+Note that each character should belong to exactly one substring in a partition.
+
+
+
+Example 1:
+
+Input: s = "abacaba"
+Output: 4
+Explanation:
+Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
+It can be shown that 4 is the minimum number of substrings needed.
+Example 2:
+
+Input: s = "ssssss"
+Output: 6
+Explanation:
+The only valid partition is ("s","s","s","s","s","s").
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only English lowercase letters.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int partitionString(string s)
+    {
+        unordered_map<char, int> mp;
+        int ans = 1;
+        for (auto ch : s)
+        {
+            if (mp.find(ch) != mp.end())
+            {
+                ans++;
+                mp.clear();
+            }
+            mp[ch]++;
+        }
+        return ans;
+    }
+};
+int main()
+{
+}
