768639908cee90ad43e141aa546a2d589993a62e
akash_ghadge <asghadge6@gmail.com>
2023-06-29 10:29:00 +0530

    LC Daily Done

---
+++ Daily Challange/06_23 June/29 864. Shortest Path to Get All Keys.cpp
@@ -0,0 +1,104 @@
+/*
+        864. Shortest Path to Get All Keys
+        
+
+        You are given an m x n grid grid where:
+
+
+	'.' is an empty cell.
+	'#' is a wall.
+	'@' is the starting point.
+	Lowercase letters represent keys.
+	Uppercase letters represent locks.
+
+
+You start at the starting point and one move consists of walking one space in one of the four cardinal directions. You cannot walk outside the grid, or walk into a wall.
+
+If you walk over a key, you can pick it up and you cannot walk over a lock unless you have its corresponding key.
+
+For some 1 <= k <= 6, there is exactly one lowercase and one uppercase letter of the first k letters of the English alphabet in the grid. This means that there is exactly one key for each lock, and one lock for each key; and also that the letters used to represent the keys and locks were chosen in the same order as the English alphabet.
+
+Return the lowest number of moves to acquire all keys. If it is impossible, return -1.
+
+ 
+Example 1:
+
+Input: grid = ["@.a..","###.#","b.A.B"]
+Output: 8
+Explanation: Note that the goal is to obtain all the keys not to open all the locks.
+
+
+Example 2:
+
+Input: grid = ["@..aA","..B#.","....b"]
+Output: 6
+
+
+Example 3:
+
+Input: grid = ["@Aa"]
+Output: -1
+
+
+ 
+Constraints:
+
+
+	m == grid.length
+	n == grid[i].length
+	1 <= m, n <= 30
+	grid[i][j] is either an English letter, '.', '#', or '@'. 
+	There is exactly one '@' in the grid.
+	The number of keys in the grid is in the range [1, 6].
+	Each key in the grid is unique.
+	Each key in the grid has a matching lock.
+        */
+        
+
+        #include <bits/stdc++.h>
+        using namespace std;
+class Solution {
+public:
+int shortestPathAllKeys(vector<string>& grid) {
+    int m=grid.size(), n=m?grid[0].size():0;
+    if(!m || !n) return 0;
+    int path=0, K=0;
+    vector<int> dirs={0,-1,0,1,0};
+    vector<vector<vector<bool>>> visited(m,vector<vector<bool>>(n,vector<bool>(64,0))); //at most 6 keys, using bitmap 111111
+    queue<pair<int,int>> q; //<postion, hold keys mapping>
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]=='@'){
+                q.push({i*n+j,0});
+                visited[i][j][0]=1;                    
+            }
+            if(grid[i][j]>='A' && grid[i][j]<='F') K++; //total alpha number
+        }
+    }
+    while(!q.empty()){
+        int size=q.size();
+        for(int i=0;i<size;i++){
+            int a=q.front().first/n, b=q.front().first%n;
+            int carry=q.front().second;
+            q.pop();        
+            if(carry==((1<<K)-1)) return path; //if all keys hold, just return 
+            for(int j=0;j<4;j++){
+                int x=a+dirs[j], y=b+dirs[j+1], k=carry;
+                if(x<0 || x>=m || y<0 || y>=n || grid[x][y]=='#') continue;
+                if(grid[x][y]>='a' && grid[x][y]<='f'){
+                    k=carry|(1<<(grid[x][y]-'a')); //update hold keys
+                }
+                else if(grid[x][y]>='A' && grid[x][y]<='F'){
+                    if(!(carry & (1<<(grid[x][y]-'A')))) continue;
+                }
+                if(!visited[x][y][k]){
+                    visited[x][y][k]=1;
+                    q.push({x*n+y,k});
+               }                
+            }
+        }
+        path++;
+    }
+    return -1;
+}
+};
